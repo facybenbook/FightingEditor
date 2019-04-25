0884ad72f889d80f4b6d56288b4b03588d94ecab
kazuma <17cu0235@gmail.com>
2019-04-26 03:47:33 +0900

    プレハブ作成

---
+++ FightEngine/Assets/CUFightingEngine/Editor/CharacterModelWindow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CharacterModelWindow : ScriptableWizard
+{
+    public static CharacterModelWindow window;
+
+    private GameObject modelObj;
+    private string charaName;
+    [MenuItem("格ゲーエンジン/キャラ作成")]
+    static void Open()
+    {
+        if (window == null)
+        {
+            window = DisplayWizard<CharacterModelWindow>("CharacterModelWindow");
+        }
+        window.createButtonName = "キャラクター作成";     
+    }
+    //ScriptableWizard用のonGUI(onGUIは内部で既に使われているため)
+    protected override bool DrawWizardGUI()
+    {
+
+        base.DrawWizardGUI();
+        modelObj = EditorGUILayout.ObjectField("キャラクターモデル",modelObj,typeof(GameObject),false)as GameObject;
+        charaName = EditorGUILayout.TextField("キャラ名",  charaName);
+        
+        return true;
+    }
+    //右下のボタンを押された時の判定
+    private void OnWizardCreate()
+    {
+        GameObject obj = PrefabUtility.InstantiatePrefab(modelObj) as GameObject;
+        PrefabUtility.SaveAsPrefabAsset(obj, "Assets/" + charaName + ".prefab");
+        CharacterView.Open();
+        Close();
+    }
+
+}
---
+++ FightEngine/Assets/CUFightingEngine/Editor/CharacterView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class CharacterView : SceneView
+{
+    public static void Open()
+    {
+        var  window = ScriptableObject.CreateInstance<CharacterView>();
+        window.Show();
+        window.pivot = new Vector3(0, 5, -10);
+        window.rotation = Quaternion.identity;
+        //ほぼカメラ位置
+        window.LookAt( window.pivot, window.rotation, 1);
+        
+    }
+}
